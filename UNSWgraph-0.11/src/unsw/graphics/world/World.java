483967bb4bbc13e81c43d56f676c68834a1d936c
JenniferCheng <jennychengjr@gmail.com>
2019-07-17 17:27:10 +1000

    Camera working for most tests, looks a bit weird on test2, will continue test it to make it more stable.

--- UNSWgraph-0.11/src/unsw/graphics/world/World.java
+++ UNSWgraph-0.11/src/unsw/graphics/world/World.java
@@ -11,10 +11,7 @@ import com.jogamp.newt.event.MouseEvent;
 import com.jogamp.newt.event.MouseListener;
 import com.jogamp.opengl.GL3;
 
-import unsw.graphics.Application3D;
-import unsw.graphics.CoordFrame3D;
-import unsw.graphics.Matrix4;
-import unsw.graphics.Shader;
+import unsw.graphics.*;
 import unsw.graphics.geometry.Point2D;
 import unsw.graphics.geometry.Point3D;
 import unsw.graphics.geometry.TriangleMesh;
@@ -22,59 +19,61 @@ import unsw.graphics.geometry.TriangleMesh;
 
 
 /**
- * COMMENT: Comment Game 
+ * COMMENT: Comment Game
  *
  * @author malcolmr
  */
 public class World extends Application3D implements MouseListener, KeyListener{
 
-    private Terrain terrain;
-    private TriangleMesh terrainMesh;
+	private Terrain terrain;
+	private TriangleMesh terrainMesh;
+	private WorldCamera myCamera;
 
-    //data so we can drag around world
-    private float rotateX = 0;
-    private float rotateY = 0;
-    private float posX = 0;
-    private float posY = 0;
-    private float posZ = -6;
-    private Point2D myMousePoint = null;
-    private static final int ROTATION_SCALE = 1;
-    
-    
-    public World(Terrain terrain) {
-    	super("Assignment 2", 800, 600);
-        this.terrain = terrain;
-   
-    }
-   
-    /**
-     * Load a level file and display it.
-     * 
-     * @param args - The first argument is a level file in JSON format
-     * @throws FileNotFoundException
-     * change run configurations
-     */
-    public static void main(String[] args) throws IOException {
-//    	use temporary json file
-		File tempForTest = new File("/Users/jennifer/Documents/Courses/comp3421/ass2/comp3421/UNSWgraph-0.11/res/worlds/test4.json");
-		Terrain terrain = LevelIO.load(tempForTest);
-//        Terrain terrain = LevelIO.load(new File(args[0]));
-        World world = new World(terrain);
-        world.start();
-    }
+	//data so we can drag around world
+	private float rotateX = 0;
+	private float rotateY = 0;
+	private float posX = 0;
+	private float posY = 0;
+	private float posZ = -4;
+	private Point2D myMousePoint = null;
+	private static final int ROTATION_SCALE = 1;
+
+
+	public World(Terrain terrain) {
+		super("Assignment 2", 800, 600);
+		this.terrain = terrain;
+
+	}
+
+	/**
+	 * Load a level file and display it.
+	 *
+	 * @param args - The first argument is a level file in JSON format
+	 * @throws FileNotFoundException
+	 * change run configurations
+	 */
+	public static void main(String[] args) throws IOException {
+////    	use temporary json file
+//		File tempForTest = new File("/Users/jennifer/Documents/Courses/comp3421/ass2/comp3421/UNSWgraph-0.11/res/worlds/test1.json");
+//		Terrain terrain = LevelIO.load(tempForTest);
+        Terrain terrain = LevelIO.load(new File(args[0]));
+		World world = new World(terrain);
+		world.start();
+	}
 
 	@Override
 	public void display(GL3 gl) {
 		super.display(gl);
 		CoordFrame3D frame = CoordFrame3D.identity()
-                .translate(posX, posY, posZ)
-                .rotateX(rotateX)
-                .rotateY(rotateY);
+				.translate(posX, posY, posZ)
+				.rotateX(rotateX)
+				.rotateY(rotateY);
 
-
-        Shader.setPenColor(gl, Color.PINK);
+		Shader.setPenColor(gl, Color.PINK);
+		this.myCamera.setView(gl);
 		this.terrainMesh.draw(gl, frame);
 		this.terrain.drawObjects(gl, frame);
+
 	}
 
 	@Override
@@ -87,114 +86,117 @@ public class World extends Application3D implements MouseListener, KeyListener{
 	@Override
 	public void init(GL3 gl) {
 		super.init(gl);
-		
 
-        getWindow().addMouseListener(this);
-        getWindow().addKeyListener(this);
-		
-		 Shader shader = new Shader(gl, "shaders/vertex_phong.glsl",
-	                "shaders/fragment_phong.glsl");
-        shader.use(gl);
 
-        // Set the lighting properties
-        Shader.setPoint3D(gl, "lightPos", new Point3D(0, 0, 5));
-        Shader.setColor(gl, "lightIntensity", Color.WHITE);
-        Shader.setColor(gl, "ambientIntensity", new Color(0.2f, 0.2f, 0.2f));
+		getWindow().addMouseListener(this);
+//		getWindow().addKeyListener(this);
 
-        // Set the material properties
-        Shader.setColor(gl, "ambientCoeff", Color.WHITE);
-        Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
-        Shader.setColor(gl, "specularCoeff", new Color(0.8f, 0.8f, 0.8f));
-        Shader.setFloat(gl, "phongExp", 16f);
-        
+		Shader shader = new Shader(gl, "shaders/vertex_phong.glsl",
+				"shaders/fragment_phong.glsl");
+		shader.use(gl);
+
+		// Set the lighting properties
+		Shader.setPoint3D(gl, "lightPos", new Point3D(0, 0, 5));
+		Shader.setColor(gl, "lightIntensity", Color.WHITE);
+		Shader.setColor(gl, "ambientIntensity", new Color(0.2f, 0.2f, 0.2f));
+
+		// Set the material properties
+		Shader.setColor(gl, "ambientCoeff", Color.WHITE);
+		Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
+		Shader.setColor(gl, "specularCoeff", new Color(0.8f, 0.8f, 0.8f));
+		Shader.setFloat(gl, "phongExp", 16f);
+
 		this.terrainMesh = terrain.makeTerrain(gl);
+		this.myCamera = new WorldCamera(terrain);
+		getWindow().addKeyListener(myCamera);
+
 	}
 
 	@Override
 	public void reshape(GL3 gl, int width, int height) {
-        super.reshape(gl, width, height);
-        Shader.setProjMatrix(gl, Matrix4.perspective(60, width/(float)height, 1, 100));
+		super.reshape(gl, width, height);
+		Shader.setProjMatrix(gl, Matrix4.perspective(60, width/(float)height, 1, 100));
 	}
-	
+
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		 myMousePoint = new Point2D(e.getX(), e.getY());
+		myMousePoint = new Point2D(e.getX(), e.getY());
 	}
 
-    @Override
-    public void mouseClicked(MouseEvent e) { }
+	@Override
+	public void mouseClicked(MouseEvent e) { }
 
-    @Override
-    public void mouseEntered(MouseEvent e) { }
+	@Override
+	public void mouseEntered(MouseEvent e) { }
 
-    @Override
-    public void mouseExited(MouseEvent e) { }
+	@Override
+	public void mouseExited(MouseEvent e) { }
 
-    @Override
-    public void mousePressed(MouseEvent e) { }
+	@Override
+	public void mousePressed(MouseEvent e) { }
 
-    @Override
-    public void mouseReleased(MouseEvent e) { }
+	@Override
+	public void mouseReleased(MouseEvent e) { }
 
-    @Override
-    public void mouseWheelMoved(MouseEvent e) { }
+	@Override
+	public void mouseWheelMoved(MouseEvent e) { }
 
-    @Override
+	@Override
 	public void mouseDragged(MouseEvent e) {
 		Point2D p = new Point2D(e.getX(), e.getY());
 
-        if (myMousePoint != null) {
-            float dx = p.getX() - myMousePoint.getX();
-            float dy = p.getY() - myMousePoint.getY();
+		if (myMousePoint != null) {
+			float dx = p.getX() - myMousePoint.getX();
+			float dy = p.getY() - myMousePoint.getY();
 
-            // Note: dragging in the x dir rotates about y
-            //       dragging in the y dir rotates about x
-            rotateY += dx * ROTATION_SCALE;
-            rotateX += dy * ROTATION_SCALE;
+			// Note: dragging in the x dir rotates about y
+			//       dragging in the y dir rotates about x
+			rotateY += dx * ROTATION_SCALE;
+			rotateX += dy * ROTATION_SCALE;
 
-        }
-        myMousePoint = p;
+		}
+		myMousePoint = p;
 	}
 
-    //helps to zoom in and out 
+	//helps to zoom in and out
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int key = e.getKeyCode();
 
-	    if (key == KeyEvent.VK_LEFT) {
-	        posX -= 1;
-	        System.out.println("hi");
-	    }
+		if (key == KeyEvent.VK_LEFT) {
+			posX -= 1;
+			System.out.println("hi");
+		}
 
-	    if (key == KeyEvent.VK_RIGHT) {
-	        posX += 1;
-	    }
+		if (key == KeyEvent.VK_RIGHT) {
+			posX += 1;
+		}
 
-	    if (key == KeyEvent.VK_UP) {
-	    	posY += 1;
-	    }
+		if (key == KeyEvent.VK_UP) {
+			posY += 1;
+		}
 
-	    if (key == KeyEvent.VK_DOWN) {
-	    	posY -= 1;
-	    }
-	    
-	    //zoom in
-	    if (key == KeyEvent.VK_X) {
-	    	posZ += 1;
-	    }
-	    
-	    //zoom out
-	    if (key == KeyEvent.VK_Z) {
-	    	posZ -= 1;
-	    }
+		if (key == KeyEvent.VK_DOWN) {
+			posY -= 1;
+		}
+
+		//zoom in
+		if (key == KeyEvent.VK_X) {
+			posZ += 1;
+		}
+
+		//zoom out
+		if (key == KeyEvent.VK_Z) {
+			posZ -= 1;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-    
+
 }
